6cf6a25d1e06edee219cca757e147a5c09f678a6
sywnplwmn <sivanflomen@gmail.com>
2022-05-13 20:35:22 +0300

    api json

--- src/main/java/com/finalProject/BOOKLUB/service/BookService.java
+++ src/main/java/com/finalProject/BOOKLUB/service/BookService.java
@@ -4,17 +4,17 @@ import com.finalProject.booklub.repository.entities.Book;
 import com.finalProject.booklub.repository.BookRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
+import java.util.Objects;
+import java.util.Optional;
 
 @Service
 public class BookService{
-    private final BookRepository bookRepository;
+    private static BookRepository bookRepository;
 
 
-   /**-
-    @param xyz
-     **/
    @Autowired
     public BookService(BookRepository bookRepository)  {
         this.bookRepository = bookRepository;
@@ -25,7 +25,31 @@ public class BookService{
         }
 
     public static void addNewBook(Book book) {
-        System.out.println(book);
+        Optional<Book> bookByOptional = bookRepository.findByTitle(book.getTitle());
+        if(bookByOptional.isPresent()){
+            throw new IllegalStateException("book already insert");
+        }
+        bookRepository.save(book);
     }
 
+    public void deleteBook(Book bookTitle) {
+       bookRepository.findByTitle(bookTitle.getTitle());
+        boolean exists = bookRepository.existsById(bookTitle.getId());
+        if(!exists){
+            throw new IllegalStateException("The book does not exist");
+        }
+        bookRepository.deleteById(bookTitle.getId());
+    }
+
+    @Transactional
+    public void updateBook(long bookI, String title, String publisher){
+       Book book = bookRepository.findById(bookI).orElseThrow(() -> new IllegalStateException("the book is not exist"));
+
+       if (title != null && title.length() > 0 && !Objects.equals(book.getTitle(), title)){
+           book.setTitle(title);
+       }
+        if (publisher != null && publisher.length()> 0 && !Objects.equals(book.getPublisherName(), publisher)){
+            book.setPublisherName(publisher);
+        }
+    }
 }
